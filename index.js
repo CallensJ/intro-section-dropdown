bda001dfdb0108972996d3bb3558a5f43e015be7
Callensj <callens.johan@protonmail.com>
2023-10-24 16:46:12 +0200

    completed mobile navbar styles

---
+++ index.js
@@ -0,0 +1,19 @@
+const toggleBtn = document.querySelector(".toggle-buttons");
+const mobileNavbar = document.querySelector("#mobile-navbar");
+const section = document.getElementById("hero-section");
+const arrows = document.getElementsByClassName(".arrow-down");
+
+toggleBtn.addEventListener("click", () => {
+  const icons = document.querySelector(".toggle-buttons img");
+  const isActive = mobileNavbar.classList.contains("active-navbar");
+  mobileNavbar.classList.toggle("active-navbar");
+  section.classList.toggle("overbackground");
+  icons.classList = isActive
+    ? (icons.src = "./images/icon-menu.svg")
+    : (icons.src = "./images/icon-close-menu.svg");
+});
+
+function toggleDropdown(id) {
+  document.getElementById(id).classList.toggle("open");
+
+}
